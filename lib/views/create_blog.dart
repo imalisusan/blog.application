44dcb5bd90558f76afd721094df03a75a8e6568d
Imali Susan <susanimali52@gmail.com>
2021-05-29 23:14:45 -0700

    Upload data to firestore

--- lib/views/create_blog.dart
+++ lib/views/create_blog.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:blog_application/services/crud.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CreateBlog extends StatefulWidget {
   @override
@@ -7,6 +12,33 @@ class CreateBlog extends StatefulWidget {
 
 class _CreateBlogState extends State<CreateBlog> {
   String authorName, title, description;
+
+  File selectedImage;
+
+  CrudMethods crudMethods = new CrudMethods();
+
+  final picker = ImagePicker();
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        selectedImage = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  uploadBlog() {
+    Map<String, String> blogMap = {
+      "authorName": authorName,
+      "title": title,
+      "description": description,
+    };
+
+    crudMethods.addData(blogMap).then((value) => Navigator.pop(context));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,9 +67,14 @@ class _CreateBlogState extends State<CreateBlog> {
         backgroundColor: Colors.transparent,
         elevation: 0.0,
         actions: <Widget>[
-          Container(
-              padding: EdgeInsets.symmetric(horizontal: 60),
-              child: Icon(Icons.file_upload))
+          GestureDetector(
+            onTap: () {
+              uploadBlog();
+            },
+            child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 60),
+                child: Icon(Icons.file_upload)),
+          )
         ],
       ),
       body: Container(
@@ -46,18 +83,34 @@ class _CreateBlogState extends State<CreateBlog> {
             SizedBox(
               height: 10,
             ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 16),
-              height: 150,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(6),
-              ),
-              width: MediaQuery.of(context).size.width,
-              child: Icon(
-                Icons.add_a_photo,
-                color: Colors.black54,
-              ),
+            GestureDetector(
+              onTap: () {},
+              child: selectedImage != null
+                  ? Container(
+                      height: 150,
+                      margin: EdgeInsets.symmetric(horizontal: 16),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(6),
+                        child: Image.file(
+                          selectedImage,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      width: MediaQuery.of(context).size.width,
+                    )
+                  : Container(
+                      margin: EdgeInsets.symmetric(horizontal: 16),
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      width: MediaQuery.of(context).size.width,
+                      child: Icon(
+                        Icons.add_a_photo,
+                        color: Colors.black54,
+                      ),
+                    ),
             ),
             SizedBox(
               height: 8,
@@ -73,7 +126,6 @@ class _CreateBlogState extends State<CreateBlog> {
                       authorName = val;
                     },
                   ),
-
                   TextField(
                     decoration: InputDecoration(hintText: "Title"),
                     style: TextStyle(fontFamily: 'Montserrat'),
@@ -81,7 +133,6 @@ class _CreateBlogState extends State<CreateBlog> {
                       title = val;
                     },
                   ),
-
                   TextField(
                     decoration: InputDecoration(hintText: "Description"),
                     style: TextStyle(fontFamily: 'Montserrat'),
