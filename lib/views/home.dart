9c23d26242b746b3ec57f2ddd1e32be89955c851
Imali Susan <susanimali52@gmail.com>
2021-05-31 21:36:33 -0700

    Fetch data from firestore

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,6 @@
+import 'package:blog_application/services/crud.dart';
 import 'package:blog_application/views/create_blog.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -7,6 +9,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  CrudMethods crudMethods = new CrudMethods();
+
+  QuerySnapshot blogsSnapshot;
+
+  Widget blogsList() {
+    return Container(
+      child: Column(
+        children: [
+          blogsSnapshot != null ? ListView.builder(
+            itemCount: blogsSnapshot.documents.length,
+            itemBuilder: (context, index) {
+              return BlogsTile(
+                authorName: blogsSnapshot.documents[index].data['authorName'],
+                title: blogsSnapshot.documents[index].data['title'],
+                description: blogsSnapshot.documents[index].data['description'],
+              );
+            },
+          ): Container(
+            child: CircularProgressIndicator(),
+            alignment: Alignment.center,)
+        ],
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    crudMethods.getData().then((result) {
+      blogsSnapshot = result;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,6 +85,41 @@ class _HomePageState extends State<HomePage> {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BlogsTile extends StatelessWidget {
+  String title, description, authorName;
+
+  BlogsTile(
+      {@required this.title,
+      @required this.description,
+      @required this.authorName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Container(
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.black45.withOpacity(0.3),
+              borderRadius: BorderRadius.circular(6),
+            ),
+          ),
+          Container(
+            child: Column(
+              children: [
+                Text(title),
+                Text(description),
+                Text(authorName),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
